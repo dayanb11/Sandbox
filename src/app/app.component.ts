3018d7aa2adb8513acae6d813fb295ea9aadcf0d
dayanb11 <dayanbeni@gmail.com>
2024-03-01 20:54:52 +0200

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {JsonPipe} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [FormsModule, ReactiveFormsModule, MatCheckboxModule,RouterOutlet ,JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Sandbox';
+   topping= this._formBuilder.group({
+    checkme: false});
+  clicked(event: Event | undefined) { 
+    alert(event?.AT_TARGET.valueOf)
+    alert ('Function not implemented.');
+   }
+  
+  constructor(private _formBuilder: FormBuilder) {
+  }
 }
