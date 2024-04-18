d8ed93ce7f3b535a2f9f86a9518d337c8e089ed1
dayanb11 <dayanbeni@gmail.com>
2024-04-18 13:01:24 +0300

    add sub-component

---
+++ src/app/sub-component/sub-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './sub-component.component.html',
+  styleUrl: './sub-component.component.css'
+})
+export class SubComponentComponent {
+
+}
